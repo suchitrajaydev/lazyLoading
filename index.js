5aac8909e942402f59c18e3b035c37d29dfb0000
Suchitra <suchitra.jayadev@gmail.com>
2018-10-29 17:03:01 +0530

    Initial check in

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+const inFile = './index.hbs';
+const outFile = './index.html';
+
+const data = require('./data.json');
+
+const source = fs.readFileSync(inFile, 'utf8');
+const template = handlebars.compile(source, { strict: true });
+const result = template(data);
+
+fs.writeFileSync(outFile, result);
+console.log(`File written to ${outFile}`);
